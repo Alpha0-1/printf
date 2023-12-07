a25c923074c14503f68e7037c5fdec9157a41c3f
Alpha0-1 <alphaomollo142@gmail.com>
2023-12-07 12:53:28 -0800

    1

---
+++ test/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point for the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Printing an integer: %d\n", 42);
+	printf("Printing another integer: %i\n", -17);
+
+	return (0);
+}
